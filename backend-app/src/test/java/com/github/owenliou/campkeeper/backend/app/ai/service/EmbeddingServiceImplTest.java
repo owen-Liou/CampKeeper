a6db611ca2fa5bc3af1930af49a1bfcc457d6c9b
owenLiou <owen.liu@udngroup.com.tw>
2026-06-10 15:22:55 +0800

    test : 新增測試

--- backend-app/src/test/java/com/github/owenliou/campkeeper/backend/app/ai/service/EmbeddingServiceImplTest.java
+++ backend-app/src/test/java/com/github/owenliou/campkeeper/backend/app/ai/service/EmbeddingServiceImplTest.java
@@ -1,21 +1,33 @@
 package com.github.owenliou.campkeeper.backend.app.ai.service;
 
 import com.github.owenliou.campkeeper.backend.app.AbstractUnitTestCase;
-import com.github.owenliou.campkeeper.backend.app.ai.service.impl.EmbeddingServiceImpl;
+import com.github.owenliou.campkeeper.model.entity.Campsite;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @Slf4j
 class EmbeddingServiceImplTest extends AbstractUnitTestCase {
 
     @Autowired
-    private EmbeddingServiceImpl embeddingServiceImpl;
+    private EmbeddingService embeddingService;
 
     @Test
     void  syncCampsitesToVectorStoreTest(){
-        int count = embeddingServiceImpl.syncCampsitesToVectorStore();
+        int count = embeddingService.syncCampsitesToVectorStore();
         log.info("完成，共同步 {} 筆向量資料", count);
+    }
+
+    @Test
+    void semanticSearchCampsitesTest(){
+        String query = "適合親子露營的營地";
+        int topK = 5;
+        List<Campsite> campsites = embeddingService.semanticSearchCamp(query, topK);
+        log.info("搜尋結果：");
+        log.info("共找到 {} 筆相關營地", campsites.size());
+        campsites.forEach(campsite -> log.info("營地ID: {}, 名稱: {}, 地址: {}", campsite.getId(), campsite.getName(), campsite.getCity()+campsite.getDistrict()));
     }
 
 }
